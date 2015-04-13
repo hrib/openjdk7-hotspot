1e4b173be01a87b3710e92a3e4e8c7efcd5db3ca
baal <hribaljaromir@gmail.com>
2015-04-13 16:44:13 +0200

    some platform specific includes replaced with x86(_32) versions part 3

--- interpreter/interpreterRuntime.hpp
+++ interpreter/interpreterRuntime.hpp
@@ -148,7 +148,60 @@ class InterpreterRuntime: AllStatic {
 
   // Platform dependent stuff
 #ifdef TARGET_ARCH_x86
-# include "interpreterRT_x86.hpp"
+//# include "interpreterRT_x86.hpp"
+#include "memory/allocation.hpp"
+
+// native method calls
+
+class SignatureHandlerGenerator: public NativeSignatureIterator {
+ private:
+  MacroAssembler* _masm;
+#ifdef AMD64
+#ifdef _WIN64
+  unsigned int _num_args;
+#else
+  unsigned int _num_fp_args;
+  unsigned int _num_int_args;
+#endif // _WIN64
+  int _stack_offset;
+#else
+  void move(int from_offset, int to_offset);
+  void box(int from_offset, int to_offset);
+#endif // AMD64
+
+  void pass_int();
+  void pass_long();
+  void pass_float();
+#ifdef AMD64
+  void pass_double();
+#endif // AMD64
+  void pass_object();
+
+ public:
+  // Creation
+  SignatureHandlerGenerator(methodHandle method, CodeBuffer* buffer) : NativeSignatureIterator(method) {
+    _masm = new MacroAssembler(buffer);
+#ifdef AMD64
+#ifdef _WIN64
+    _num_args = (method->is_static() ? 1 : 0);
+    _stack_offset = (Argument::n_int_register_parameters_c+1)* wordSize; // don't overwrite return address
+#else
+    _num_int_args = (method->is_static() ? 1 : 0);
+    _num_fp_args = 0;
+    _stack_offset = wordSize; // don't overwrite return address
+#endif // _WIN64
+#endif // AMD64
+  }
+
+  // Code generation
+  void generate(uint64_t fingerprint);
+
+  // Code generation support
+  static Register from();
+  static Register to();
+  static Register temp();
+};
+
 #endif
 #ifdef TARGET_ARCH_sparc
 # include "interpreterRT_sparc.hpp"
