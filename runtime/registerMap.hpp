f0b6573142891ae9ff3921aaebeefb05987afdab
baal <hribaljaromir@gmail.com>
2015-04-14 20:21:03 +0200

    some platform specific includes replaced with x86(_32) versions part 7

--- runtime/registerMap.hpp
+++ runtime/registerMap.hpp
@@ -136,7 +136,23 @@ class RegisterMap : public StackObj {
 
   // the following contains the definition of pd_xxx methods
 #ifdef TARGET_ARCH_x86
-# include "registerMap_x86.hpp"
+//# include "registerMap_x86.hpp"
+// machine-dependent implemention for register maps
+  friend class frame;
+
+ private:
+  // This is the hook for finding a register in an "well-known" location,
+  // such as a register block of a predetermined format.
+  // Since there is none, we just return NULL.
+  // See registerMap_sparc.hpp for an example of grabbing registers
+  // from register save areas of a standard layout.
+   address pd_location(VMReg reg) const {return NULL;}
+
+  // no PD state to clear or copy:
+  void pd_clear() {}
+  void pd_initialize() {}
+  void pd_initialize_from(const RegisterMap* map) {}
+
 #endif
 #ifdef TARGET_ARCH_sparc
 # include "registerMap_sparc.hpp"
--- runtime/stubRoutines.hpp
+++ runtime/stubRoutines.hpp
@@ -100,7 +100,31 @@ class StubRoutines: AllStatic {
   // Dependencies
   friend class StubGenerator;
 #ifdef TARGET_ARCH_MODEL_x86_32
-# include "stubRoutines_x86_32.hpp"
+//# include "stubRoutines_x86_32.hpp"
+// This file holds the platform specific parts of the StubRoutines
+// definition. See stubRoutines.hpp for a description on how to
+// extend it.
+
+enum platform_dependent_constants {
+  code_size1 =  9000,           // simply increase if too small (assembler will crash if too small)
+  code_size2 = 22000            // simply increase if too small (assembler will crash if too small)
+};
+
+class x86 {
+ friend class StubGenerator;
+ friend class VMStructs;
+
+ private:
+  static address _verify_mxcsr_entry;
+  static address _verify_fpu_cntrl_wrd_entry;
+
+ public:
+  static address verify_mxcsr_entry()                        { return _verify_mxcsr_entry; }
+  static address verify_fpu_cntrl_wrd_entry()                { return _verify_fpu_cntrl_wrd_entry; }
+};
+
+  static bool    returns_to_call_stub(address return_pc)     { return return_pc == _call_stub_return_address; }
+
 #endif
 #ifdef TARGET_ARCH_MODEL_x86_64
 # include "stubRoutines_x86_64.hpp"
